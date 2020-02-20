01b541f93c28af7307af0b61b23988919763e161
Chris McGinn <cffionn@gmail.com>
2020-02-20 11:05:20 -0700

    Updated w/ jet-gamma deltaPhi

--- src/gdjNTupleToHist.C
+++ src/gdjNTupleToHist.C
@@ -20,6 +20,7 @@
 #include "include/centralityFromInput.h"
 #include "include/checkMakeDir.h"
 #include "include/configParser.h"
+#include "include/etaPhiFunc.h"
 #include "include/getLinBins.h"
 #include "include/getLogBins.h"
 #include "include/ghostUtil.h"
@@ -74,14 +75,14 @@ int gdjNTupleToHist(std::string inConfigFileName)
   }
 
   const Int_t nMaxPtBins = 200;
-  const Int_t nPtBins = std::stoi(config.GetConfigVal("NPTBINS"));
-  if(!goodBinning(inConfigFileName, nMaxPtBins, nPtBins, "NPTBINS")) return 1;
-  const Float_t ptBinsLow = std::stof(config.GetConfigVal("PTBINSLOW"));
-  const Float_t ptBinsHigh = std::stof(config.GetConfigVal("PTBINSHIGH"));
-  const Bool_t ptBinsDoLog = std::stof(config.GetConfigVal("PTBINSDOLOG"));
-  Double_t ptBins[nMaxPtBins+1];
-  if(ptBinsDoLog) getLogBins(ptBinsLow, ptBinsHigh, nPtBins, ptBins);
-  else getLinBins(ptBinsLow, ptBinsHigh, nPtBins, ptBins);
+  const Int_t nGammaPtBins = std::stoi(config.GetConfigVal("NGAMMAPTBINS"));
+  if(!goodBinning(inConfigFileName, nMaxPtBins, nGammaPtBins, "NGAMMAPTBINS")) return 1;
+  const Float_t gammaPtBinsLow = std::stof(config.GetConfigVal("GAMMAPTBINSLOW"));
+  const Float_t gammaPtBinsHigh = std::stof(config.GetConfigVal("GAMMAPTBINSHIGH"));
+  const Bool_t gammaPtBinsDoLog = std::stof(config.GetConfigVal("GAMMAPTBINSDOLOG"));
+  Double_t gammaPtBins[nMaxPtBins+1];
+  if(gammaPtBinsDoLog) getLogBins(gammaPtBinsLow, gammaPtBinsHigh, nGammaPtBins, gammaPtBins);
+  else getLinBins(gammaPtBinsLow, gammaPtBinsHigh, nGammaPtBins, gammaPtBins);
 
   const Int_t nMaxEtaPhiBins = 100;
   const Int_t nEtaBins = std::stoi(config.GetConfigVal("NETABINS"));
@@ -105,28 +106,28 @@ int gdjNTupleToHist(std::string inConfigFileName)
   getLinBins(-TMath::Pi()+0.01, TMath::Pi()+0.01, nPhiBins, phiBins);
 
   //Pt sub bins handling
-  const Int_t nPtBinsSub = std::stoi(config.GetConfigVal("NPTBINSSUB"));
-  if(!goodBinning(inConfigFileName, nMaxSubBins, nPtBinsSub, "NPTBINSSUB")) return 1;
-  const Float_t ptBinsSubLow = std::stof(config.GetConfigVal("PTBINSLOW"));
-  const Float_t ptBinsSubHigh = std::stof(config.GetConfigVal("PTBINSHIGH"));
-  if(ptBinsSubLow < ptBinsLow) std::cout << "ERROR - config \'" << inConfigFileName << "\' contains ptBinsSubLow \'" << ptBinsSubLow << "\' less than ptBinsLow \'" << ptBinsLow << "\'. return 1" << std::endl;
-  if(ptBinsSubHigh > ptBinsHigh) std::cout << "ERROR - config \'" << inConfigFileName << "\' contains ptBinsSubHigh \'" << ptBinsSubHigh << "\' greater than ptBinsHigh \'" << ptBinsHigh << "\'. return 1" << std::endl;
-  if(ptBinsSubLow < ptBinsLow || ptBinsSubHigh > ptBinsHigh) return 1;
-  const Bool_t ptBinsSubDoLog = std::stof(config.GetConfigVal("PTBINSDOLOG"));
-  Double_t ptBinsSub[nMaxSubBins+1];
-  if(ptBinsSubDoLog) getLogBins(ptBinsSubLow, ptBinsSubHigh, nPtBinsSub, ptBinsSub);
-  else getLinBins(ptBinsSubLow, ptBinsSubHigh, nPtBinsSub, ptBinsSub);
-  std::vector<std::string> ptBinsSubStr;
-  for(Int_t pI = 0; pI < nPtBinsSub; ++pI){
-    ptBinsSubStr.push_back("Pt" + prettyString(ptBinsSub[pI], 1, true) + "to" + prettyString(ptBinsSub[pI+1], 1, true));
+  const Int_t nGammaPtBinsSub = std::stoi(config.GetConfigVal("NGAMMAPTBINSSUB"));
+  if(!goodBinning(inConfigFileName, nMaxSubBins, nGammaPtBinsSub, "NGAMMAPTBINSSUB")) return 1;
+  const Float_t gammaPtBinsSubLow = std::stof(config.GetConfigVal("GAMMAPTBINSSUBLOW"));
+  const Float_t gammaPtBinsSubHigh = std::stof(config.GetConfigVal("GAMMAPTBINSSUBHIGH"));
+  if(gammaPtBinsSubLow < gammaPtBinsLow) std::cout << "ERROR - config \'" << inConfigFileName << "\' contains gammaPtBinsSubLow \'" << gammaPtBinsSubLow << "\' less than gammaPtBinsLow \'" << gammaPtBinsLow << "\'. return 1" << std::endl;
+  if(gammaPtBinsSubHigh > gammaPtBinsHigh) std::cout << "ERROR - config \'" << inConfigFileName << "\' contains gammaPtBinsSubHigh \'" << gammaPtBinsSubHigh << "\' greater than gammaPtBinsHigh \'" << gammaPtBinsHigh << "\'. return 1" << std::endl;
+  if(gammaPtBinsSubLow < gammaPtBinsLow || gammaPtBinsSubHigh > gammaPtBinsHigh) return 1;
+  const Bool_t gammaPtBinsSubDoLog = std::stof(config.GetConfigVal("GAMMAPTBINSDOLOG"));
+  Double_t gammaPtBinsSub[nMaxSubBins+1];
+  if(gammaPtBinsSubDoLog) getLogBins(gammaPtBinsSubLow, gammaPtBinsSubHigh, nGammaPtBinsSub, gammaPtBinsSub);
+  else getLinBins(gammaPtBinsSubLow, gammaPtBinsSubHigh, nGammaPtBinsSub, gammaPtBinsSub);
+  std::vector<std::string> gammaPtBinsSubStr;
+  for(Int_t pI = 0; pI < nGammaPtBinsSub; ++pI){
+    gammaPtBinsSubStr.push_back("GammaPt" + prettyString(gammaPtBinsSub[pI], 1, true) + "to" + prettyString(gammaPtBinsSub[pI+1], 1, true));
   }
-  ptBinsSubStr.push_back("Pt" + prettyString(ptBinsSub[0], 1, true) + "to" + prettyString(ptBinsSub[nPtBinsSub], 1, true));
+  gammaPtBinsSubStr.push_back("GammaPt" + prettyString(gammaPtBinsSub[0], 1, true) + "to" + prettyString(gammaPtBinsSub[nGammaPtBinsSub], 1, true));
 
   //Eta sub bins handling
   const Int_t nEtaBinsSub = std::stoi(config.GetConfigVal("NETABINSSUB"));
   if(!goodBinning(inConfigFileName, nMaxSubBins, nEtaBinsSub, "NETABINSSUB")) return 1;
-  const Float_t etaBinsSubLow = std::stof(config.GetConfigVal("ETABINSLOW"));
-  const Float_t etaBinsSubHigh = std::stof(config.GetConfigVal("ETABINSHIGH"));
+  const Float_t etaBinsSubLow = std::stof(config.GetConfigVal("ETABINSSUBLOW"));
+  const Float_t etaBinsSubHigh = std::stof(config.GetConfigVal("ETABINSSUBHIGH"));
   if(etaBinsSubLow < etaBinsLow) std::cout << "ERROR - config \'" << inConfigFileName << "\' contains etaBinsSubLow \'" << etaBinsSubLow << "\' less than etaBinsLow \'" << etaBinsLow << "\'. return 1" << std::endl;
   if(etaBinsSubHigh > etaBinsHigh) std::cout << "ERROR - config \'" << inConfigFileName << "\' contains etaBinsSubHigh \'" << etaBinsSubHigh << "\' greater than etaBinsHigh \'" << etaBinsHigh << "\'. return 1" << std::endl;
   if(etaBinsSubLow < etaBinsLow || etaBinsSubHigh > etaBinsHigh) return 1;
@@ -144,6 +145,12 @@ int gdjNTupleToHist(std::string inConfigFileName)
     etaBinsSubStr.push_back(preStr + "Eta" + prettyString(etaBinsSub[eI], 2, true) + "to" + prettyString(etaBinsSub[eI+1], 2, true));
   }
   etaBinsSubStr.push_back(preStr + "Eta" + prettyString(etaBinsSub[0], 2, true) + "to" + prettyString(etaBinsSub[nEtaBinsSub], 2, true));
+
+  const Float_t jtPtBinsLow = std::stof(config.GetConfigVal("JTPTBINSLOW"));
+  const Float_t jtEtaBinsLow = std::stof(config.GetConfigVal("JTETABINSLOW"));
+  const Float_t jtEtaBinsHigh = std::stof(config.GetConfigVal("JTETABINSHIGH"));
+
+  const Int_t nDPhiBins = std::stoi(config.GetConfigVal("NDPHIBINS"));
   
   TFile* outFile_p = new TFile(outFileName.c_str(), "RECREATE");
   TH1F* centrality_p = nullptr;
@@ -152,6 +159,7 @@ int gdjNTupleToHist(std::string inConfigFileName)
   TH1F* photonPhiVCentPt_p[nMaxCentBins][nMaxSubBins+1];
   TH2F* photonEtaPt_p[nMaxCentBins];
   TH2F* photonEtaPhiVCentPt_p[nMaxCentBins][nMaxSubBins+1];
+  TH1F* photonJetDPhiVCentPt_p[nMaxCentBins][nMaxSubBins+1];
   
   if(doGlobalDebug) std::cout << "GLOBAL DEBUG FILE, LINE: " << __FILE__ << ", " << __LINE__ << std::endl;
 
@@ -162,21 +170,24 @@ int gdjNTupleToHist(std::string inConfigFileName)
 
   for(Int_t cI = 0; cI < nCentBins; ++cI){
     for(Int_t eI = 0; eI < nEtaBinsSub+1; ++eI){
-      photonPtVCentEta_p[cI][eI] = new TH1F(("photonPtVCentEta" + centBinsStr[cI] + "_" + etaBinsSubStr[eI]+ "_h").c_str(), ";#gamma p_{T} [GeV];Counts", nPtBins, ptBins);
+      photonPtVCentEta_p[cI][eI] = new TH1F(("photonPtVCentEta_" + centBinsStr[cI] + "_" + etaBinsSubStr[eI]+ "_h").c_str(), ";#gamma p_{T} [GeV];Counts", nGammaPtBins, gammaPtBins);
       centerTitles(photonPtVCentEta_p[cI][eI]);
     }
 
-    for(Int_t pI = 0; pI < nPtBinsSub+1; ++pI){
-      photonEtaVCentPt_p[cI][pI] = new TH1F(("photonEtaVCentPt" + centBinsStr[cI] + "_" + ptBinsSubStr[pI] + "_h").c_str(), ";#gamma #eta;Counts", nEtaBins, etaBins);
-      photonPhiVCentPt_p[cI][pI] = new TH1F(("photonPhi_" + centBinsStr[cI] + "_" + ptBinsSubStr[pI] + "_h").c_str(), ";#gamma #phi;Counts", nPhiBins, phiBins);
+    for(Int_t pI = 0; pI < nGammaPtBinsSub+1; ++pI){
+      photonEtaVCentPt_p[cI][pI] = new TH1F(("photonEtaVCentPt_" + centBinsStr[cI] + "_" + gammaPtBinsSubStr[pI] + "_h").c_str(), ";#gamma #eta;Counts", nEtaBins, etaBins);
+      photonPhiVCentPt_p[cI][pI] = new TH1F(("photonPhiVCentPt_" + centBinsStr[cI] + "_" + gammaPtBinsSubStr[pI] + "_h").c_str(), ";#gamma #phi;Counts", nPhiBins, phiBins);
 
-      centerTitles({photonEtaVCentPt_p[cI][pI], photonPhiVCentPt_p[cI][pI]});
+      photonJetDPhiVCentPt_p[cI][pI] = new TH1F(("photonJetDPhiVCentPt_" + centBinsStr[cI] + "_" + gammaPtBinsSubStr[pI] + "_h").c_str(), ";#Delta#phi_{#gamma,jet};#frac{1}{N_{#gamma}} #frac{dN_{#gamma,jet}}{d#Delta#phi_{#gamma,jet}}", nDPhiBins, 0, TMath::Pi() + 0.01);
+      
+      centerTitles({photonEtaVCentPt_p[cI][pI], photonPhiVCentPt_p[cI][pI], photonJetDPhiVCentPt_p[cI][pI]});
+      setSumW2(photonJetDPhiVCentPt_p[cI][pI]);
     }
     
-    photonEtaPt_p[cI] = new TH2F(("photonEtaPt_" + centBinsStr[cI] + "_h").c_str(), ";#gamma #eta;#gamma p_{T} [GeV]", nEtaBins, etaBins, nPtBins, ptBins);
+    photonEtaPt_p[cI] = new TH2F(("photonEtaPt_" + centBinsStr[cI] + "_h").c_str(), ";#gamma #eta;#gamma p_{T} [GeV]", nEtaBins, etaBins, nGammaPtBins, gammaPtBins);
 
-    for(Int_t pI = 0; pI < nPtBinsSub+1; ++pI){
-      photonEtaPhiVCentPt_p[cI][pI] = new TH2F(("photonEtaPhi_" + centBinsStr[cI] + "_" + ptBinsSubStr[pI] + "_h").c_str(), ";#gamma #eta;#gamma #phi", nEtaBins, etaBins, nPhiBins, phiBins);
+    for(Int_t pI = 0; pI < nGammaPtBinsSub+1; ++pI){
+      photonEtaPhiVCentPt_p[cI][pI] = new TH2F(("photonEtaPhi_" + centBinsStr[cI] + "_" + gammaPtBinsSubStr[pI] + "_h").c_str(), ";#gamma #eta;#gamma #phi", nEtaBins, etaBins, nPhiBins, phiBins);
       centerTitles(photonEtaPhiVCentPt_p[cI][pI]);
     }
     
@@ -222,6 +233,12 @@ int gdjNTupleToHist(std::string inConfigFileName)
   const ULong64_t nEntries = inTree_p->GetEntries();
   const ULong64_t nDiv = TMath::Max((ULong64_t)1, nEntries/20);
 
+  std::vector<Double_t> gammaCounts;
+  for(Int_t pI = 0; pI < nGammaPtBinsSub+1; ++pI){
+    gammaCounts.push_back(0.0);
+  }
+
+  
   std::cout << "Processing " << nEntries << " events..." << std::endl;
   for(ULong64_t entry = 0; entry < nEntries; ++entry){
     if(entry%nDiv == 0) std::cout << " Entry " << entry << "/" << nEntries << "..." << std::endl;
@@ -229,20 +246,26 @@ int gdjNTupleToHist(std::string inConfigFileName)
 
     Int_t centPos = -1;
     if(!isPP){
-      Double_t cent = centTable.GetCent(fcalA_et + fcalC_et);
+     Double_t cent = centTable.GetCent(fcalA_et + fcalC_et);
       centPos = ghostPos(centBins, cent, false);
       centrality_p->Fill(cent);      
     }
     else centPos = 0;
 
     for(unsigned int pI = 0; pI < photon_pt_p->size(); ++pI){
-      if(photon_pt_p->at(pI) < ptBins[0]) continue;
-      if(photon_pt_p->at(pI) >= ptBins[nPtBins]) continue;
-      if(photon_eta_p->at(pI) <= etaBins[0]) continue;
-      if(photon_eta_p->at(pI) >= etaBins[nEtaBins]) continue;
+      if(photon_pt_p->at(pI) < gammaPtBins[0]) continue;
+      if(photon_pt_p->at(pI) >= gammaPtBins[nGammaPtBins]) continue;
 
-      Int_t ptPos = ghostPos(nPtBinsSub, ptBinsSub, photon_pt_p->at(pI), false);
-      Int_t etaPos = ghostPos(nEtaBinsSub, etaBinsSub, photon_eta_p->at(pI), false);
+      Float_t etaValMain = photon_eta_p->at(pI);
+      Float_t etaValSub = etaValMain;
+      if(etaBinsDoAbs) etaValMain = TMath::Abs(etaValMain);
+      if(etaBinsSubDoAbs) etaValSub = TMath::Abs(etaValSub);
+
+      if(etaValMain <= etaBins[0]) continue;
+      if(etaValMain >= etaBins[nEtaBins]) continue;
+      
+      Int_t ptPos = ghostPos(nGammaPtBinsSub, gammaPtBinsSub, photon_pt_p->at(pI), false);
+      Int_t etaPos = ghostPos(nEtaBinsSub, etaBinsSub, etaValSub, false);
 
       if(etaPos >= 0){
 	photonPtVCentEta_p[centPos][etaPos]->Fill(photon_pt_p->at(pI));
@@ -250,16 +273,33 @@ int gdjNTupleToHist(std::string inConfigFileName)
       }
 
       if(ptPos >= 0){
-	photonEtaVCentPt_p[centPos][ptPos]->Fill(photon_eta_p->at(pI));
-	photonPhiVCentPt_p[centPos][ptPos]->Fill(photon_phi_p->at(pI));
-	photonEtaPhiVCentPt_p[centPos][ptPos]->Fill(photon_eta_p->at(pI), photon_phi_p->at(pI));
+	++(gammaCounts[ptPos]);
+	++(gammaCounts[nGammaPtBinsSub]);
 
-      	photonEtaVCentPt_p[centPos][nPtBinsSub]->Fill(photon_eta_p->at(pI));
-	photonPhiVCentPt_p[centPos][nPtBinsSub]->Fill(photon_phi_p->at(pI));
-	photonEtaPhiVCentPt_p[centPos][nPtBinsSub]->Fill(photon_eta_p->at(pI), photon_phi_p->at(pI));
+	photonEtaVCentPt_p[centPos][ptPos]->Fill(etaValMain);
+	photonPhiVCentPt_p[centPos][ptPos]->Fill(photon_phi_p->at(pI));
+	photonEtaPhiVCentPt_p[centPos][ptPos]->Fill(etaValMain, photon_phi_p->at(pI));
+
+      	photonEtaVCentPt_p[centPos][nGammaPtBinsSub]->Fill(etaValMain);
+	photonPhiVCentPt_p[centPos][nGammaPtBinsSub]->Fill(photon_phi_p->at(pI));
+	photonEtaPhiVCentPt_p[centPos][nGammaPtBinsSub]->Fill(etaValMain, photon_phi_p->at(pI));
+
+	for(unsigned int jI = 0; jI < akt4hi_em_xcalib_jet_pt_p->size(); ++jI){
+	  if(akt4hi_em_xcalib_jet_pt_p->at(jI) < jtPtBinsLow) continue;
+	  if(akt4hi_em_xcalib_jet_eta_p->at(jI) <= jtEtaBinsLow) continue;
+	  if(akt4hi_em_xcalib_jet_eta_p->at(jI) >= jtEtaBinsHigh) continue;
+
+	  Float_t dR = getDR(akt4hi_em_xcalib_jet_eta_p->at(jI), akt4hi_em_xcalib_jet_phi_p->at(jI), photon_eta_p->at(pI), photon_phi_p->at(pI));
+	  if(dR < 0.3) continue;
+	  
+	  Float_t dPhi = TMath::Abs(getDPHI(akt4hi_em_xcalib_jet_phi_p->at(jI), photon_phi_p->at(pI)));
+
+	  photonJetDPhiVCentPt_p[centPos][ptPos]->Fill(dPhi);
+	  photonJetDPhiVCentPt_p[centPos][nGammaPtBinsSub]->Fill(dPhi);
+	}	
       }
       
-      photonEtaPt_p[centPos]->Fill(photon_eta_p->at(pI), photon_pt_p->at(pI));
+      photonEtaPt_p[centPos]->Fill(etaValMain, photon_pt_p->at(pI));
     }
   }  
   
@@ -269,6 +309,23 @@ int gdjNTupleToHist(std::string inConfigFileName)
 
   outFile_p->cd();
 
+  //Pre-write and delete some of these require some mods
+  for(Int_t cI = 0; cI < nCentBins; ++cI){
+    for(Int_t pI = 0; pI < nGammaPtBinsSub+1; ++pI){
+      photonJetDPhiVCentPt_p[cI][pI]->Scale(1./gammaCounts[pI]);
+      
+      for(Int_t bIX = 0; bIX < photonJetDPhiVCentPt_p[cI][pI]->GetXaxis()->GetNbins(); ++bIX){
+	Double_t width = photonJetDPhiVCentPt_p[cI][pI]->GetXaxis()->GetBinWidth(bIX+1);
+	Double_t newVal = photonJetDPhiVCentPt_p[cI][pI]->GetBinContent(bIX+1)/width;
+	Double_t newErr = photonJetDPhiVCentPt_p[cI][pI]->GetBinError(bIX+1)/width;
+	
+	photonJetDPhiVCentPt_p[cI][pI]->SetBinContent(bIX+1, newVal);
+	photonJetDPhiVCentPt_p[cI][pI]->SetBinError(bIX+1, newErr);
+      }
+    }
+  }
+  
+  
   if(!isPP) centrality_p->Write("", TObject::kOverwrite);
 
   if(doGlobalDebug) std::cout << "GLOBAL DEBUG FILE, LINE: " << __FILE__ << ", " << __LINE__ << std::endl;
@@ -283,14 +340,15 @@ int gdjNTupleToHist(std::string inConfigFileName)
 
     if(doGlobalDebug) std::cout << "GLOBAL DEBUG FILE, LINE: " << __FILE__ << ", " << __LINE__ << std::endl;
 
-    for(Int_t pI = 0; pI < nPtBinsSub+1; ++pI){
+    for(Int_t pI = 0; pI < nGammaPtBinsSub+1; ++pI){
       photonEtaVCentPt_p[cI][pI]->Write("", TObject::kOverwrite);
       photonPhiVCentPt_p[cI][pI]->Write("", TObject::kOverwrite);
+      photonJetDPhiVCentPt_p[cI][pI]->Write("", TObject::kOverwrite);
     }
     
     photonEtaPt_p[cI]->Write("", TObject::kOverwrite);
 
-    for(Int_t pI = 0; pI < nPtBinsSub+1; ++pI){
+    for(Int_t pI = 0; pI < nGammaPtBinsSub+1; ++pI){
       photonEtaPhiVCentPt_p[cI][pI]->Write("", TObject::kOverwrite);
     }
 
@@ -308,14 +366,15 @@ int gdjNTupleToHist(std::string inConfigFileName)
       delete photonPtVCentEta_p[cI][eI];
     }
 
-    for(Int_t pI = 0; pI < nPtBinsSub+1; ++pI){
+    for(Int_t pI = 0; pI < nGammaPtBinsSub+1; ++pI){
       delete photonEtaVCentPt_p[cI][pI];
       delete photonPhiVCentPt_p[cI][pI];
+      delete photonJetDPhiVCentPt_p[cI][pI];
     }
 
     delete photonEtaPt_p[cI];
 
-    for(Int_t pI = 0; pI < nPtBinsSub+1; ++pI){
+    for(Int_t pI = 0; pI < nGammaPtBinsSub+1; ++pI){
       delete photonEtaPhiVCentPt_p[cI][pI];
     }
   }
