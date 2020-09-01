7f548ab919138c622901bebfa23547239a4b34bd
cmcginn <christopher.mc.ginn@cern.ch>
2020-09-01 18:20:00 -0400

    Cleaning some old configurations

---
+++ bash/runTest.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#JETR,INRVAL
+
+DATE=`date +%Y%m%d`
+
+mkdir -p logs/$DATE/
+
+rs=(4 2)
+files=(ntupleToHist_PbPbMC_TEST ntupleToHist_PbPbData_TEST)
+
+for i in ${rs[@]}
+do
+    for j in ${files[@]}
+    do
+	cp input/ntupleToHist/$j.config  input/ntupleToHist/"$j"_R$i.config 
+	sed -i -e "s@INRVAL@$i@g" input/ntupleToHist/"$j"_R$i.config
+
+	./bin/gdjNTupleToHist.exe input/ntupleToHist/"$j"_R$i.config &> logs/$DATE/"$j"_R"$i"_$DATE.log &
+
+#	./bin/gdjNTupleToHist.exe input/ntupleToHist/ntupleToHist_PPMC.txt &> logs/$DATE/ntupleToHist_PPMC_$DATE.log &
+#    ./bin/gdjNTupleToHist.exe input/ntupleToHist/ntupleToHist_PPData.txt &> logs/$DATE/ntupleToHist_PPData_$DATE.log &
+#    ./bin/gdjNTupleToHist.exe input/ntupleToHist/ntupleToHist_PbPbData.txt &> logs/$DATE/ntupleToHist_PbPbData_$DATE.log &
+    done
+    
+    wait
+done
+
+echo "RUNALL.SH COMPLETE!"
