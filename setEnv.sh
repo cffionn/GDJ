97a615fb940b5f1b52a6596dcc1fa6d5f98d3c8a
Chris McGinn <cffionn@gmail.com>
2021-01-15 16:14:52 -0700

    setenv missed on last commit for RooUnfold dir

--- setEnv.sh
+++ setEnv.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-GDJDIR=/usatlas/u/cfmcginn/Projects/GDJ/
+GDJDIR=/home/cfmcginn/Projects/GDJ/
+GDJDIRLIB="$GDJDIR"lib
+ROOUNFOLDDIR=/home/cfmcginn/Packages/RooUnfold/RooUnfold-build/
+
 if [[ -d $GDJDIR ]]
 then
     echo "GDJDIR set to '$GDJDIR'; if wrong please fix"
     export GDJDIR=$GDJDIR
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GDJDIR/lib
+
+    
+    if [[ $LD_LIBRARY_PATH == *"$GDJDIRLIB"* ]]
+    then
+	dummy=0
+    else
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GDJDIRLIB
+    fi
+
+    if [[ -d $ROOUNFOLDDIR ]]
+    then
+	export ROOUNFOLDDIR=$ROOUNFOLDDIR
+	
+	if [[ $LD_LIBRARY_PATH == *"$ROOUNFOLDDIR"* ]]
+	then
+	    dummy=0
+	else
+	    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOUNFOLDDIR
+	fi
+    else
+	echo "NOTE: ROOUNFOLDDIR MISSING"	
+    fi    
 else
     echo "GDJDIR given, '$GDJDIR' not found!!! Please fix" 
 fi
