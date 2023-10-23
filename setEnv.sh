26411a4f3a7c4db744c680142a3dfdfff155b26f
Chris McGinn <cffionn@gmail.com>
2023-10-23 15:56:10 +0200

    Commit before synching acf and local builds; local commit first

--- setEnv.sh
+++ setEnv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-GDJDIR=/usatlas/u/cfmcginn/Projects/GDJ/
+GDJDIR=/home/cfm/Projects/GDJ/
 GDJDIRLIB="$GDJDIR"lib
-ROOUNFOLDDIR=$GDJDIR
+ROOUNFOLDDIR=/home/cfm/Packages/RooUnfold/RooUnfold-build/
 HEPMCLIB=/home/cfm/Packages/HepMC2/hepmc-install/lib/
 #THIS IS A DUMMY FOR MAKE - UNFOLD HAPPENS OFFLINE
 #ROOUNFOLDDIR=$GDJDIR
